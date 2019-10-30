96d2c08206d2dd813b87b0bac510e2dda3800af4
Tass2T <brunog90@gmail.com>
2019-10-30 17:10:40 +0100

    footer done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import Footer from './components/Footer'
+
 class App extends React.Component {
-  
   render (){
   return (
     <div className="App">
-      
+      <Footer></Footer>
     </div>
   )}
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Footer.css'
+import Demarche from "./img/demarche-ecologique.png"
+
+
+class Footer extends React.Component {
+
+    handleEvent = (event) => {
+        alert("salut!")
+    }
+
+    render() {
+        return (
+            <div className="footer">
+                <div className="contenu">
+                    <span className="copyrigth">@2019 LE MAUVAIS COIN</span>
+                    <span className="mentionLégale" onClick={this.handleEvent}>-Mention Légale-</span>
+                </div>
+                <a className="lienEco" href="https://www.generation-net.org/fr/demarche-ecologique.html"><img src={Demarche} alt="démarche écologique"></img></a>
+            </div>
+        )
+    }
+}
+
+export default Footer
