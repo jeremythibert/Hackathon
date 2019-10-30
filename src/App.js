60d0e376619b8f9240634ef1342d8998eea5e5e8
Tass2T <brunog90@gmail.com>
2019-10-30 18:49:23 +0100

    body ready

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 
 class App extends React.Component {
-  render (){
-  return (
-    <div className="App">
-<Header />
-      <Footer></Footer>
-    </div>
-  )}
+  render() {
+    return (
+      <div className="App">
+        <div className="fond">
+          <Header />
+          <Footer></Footer></div>
+      </div>
+    )
+  }
 }
 
 export default App;
