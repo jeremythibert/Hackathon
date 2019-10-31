ec067cfc8cd93de392097473bc1d24947549ff5d
Barbara Paul <barbara.kikou@gmail.com>
2019-10-31 06:54:19 +0100

    changed the tasks

--- src/components/Taches.js
+++ src/components/Taches.js
@@ -26,16 +26,16 @@ class Taches extends React.Component{
         
         return(
             <div className="tableau">
-                <div className="one"><input className="button" type="button" name="" value="chercher-vos-enfants" onClick={this.handleChange}></input></div>
-                <div className="two"><input className="button" type="button" name="" value="tuer vos ennemis" onClick={this.handleChange}></input></div>
-                <div className="three"><input className="button" type="button" name="" value="faire votre ménage" onClick={this.handleChange}></input></div>
-                <div className="four"><input className="button" type="button" name="" value="kidnapper des gens" onClick={this.handleChange}></input></div>
-                <div className="five"><input className="button" type="button" name="" value="faire vos courses" onClick={this.handleChange}></input></div>
-                <div className="six"><input className="button" type="button" name="" value="garder vos enfants" onClick={this.handleChange}></input></div>
-                <div className="seven"><input className="button" type="button" name="" value="harceler quelqu'un" onClick={this.handleChange}></input></div>
+                <div className="one"><input className="button" type="button" name="cherchervosenfants" value="chercher vos enfants" onClick={this.handleChange}></input></div>
+                <div className="two"><input className="button" type="button" name="tuervosennemis" value="tuer vos ennemis" onClick={this.handleChange}></input></div>
+                <div className="three"><input className="button" type="button" name="fairevotremenage" value="faire votre ménage" onClick={this.handleChange}></input></div>
+                <div className="four"><input className="button" type="button" name="kidnapperdesgens" value="kidnapper des gens" onClick={this.handleChange}></input></div>
+                <div className="five"><input className="button" type="button" name="fairevoscourses" value="faire vos courses" onClick={this.handleChange}></input></div>
+                <div className="six"><input className="button" type="button" name="gardervosenfants" value="garder vos enfants" onClick={this.handleChange}></input></div>
+                <div className="seven"><input className="button" type="button" name="harcelerquelquun" value="harceler quelqu'un" onClick={this.handleChange}></input></div>
                 <div className="eigth"><input className="button" type="button" name="sortirlechien" value="sortir le chien" onClick={this.handleChange}></input></div>
-                <div className="nine"><input className="button" type="button" name="" value="coiffure" onClick={this.handleChange}></input></div>
-                <div className="ten"><input className="button" type="button" name="" value="torturer vos proches" onClick={this.handleChange}></input></div>
+                <div className="nine"><input className="button" type="button" name="coiffure" value="coiffure" onClick={this.handleChange}></input></div>
+                <div className="ten"><input className="button" type="button" name="torturervosproches" value="torturer vos proches" onClick={this.handleChange}></input></div>
             </div>
         )
     }
