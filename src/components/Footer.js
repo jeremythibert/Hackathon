f950490066fadf89af721461731c4d437ece7363
Cyril Carral <cyril.carral85@gmail.com>
2019-10-31 04:21:44 +0100

    popup done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,34 @@
 import React from 'react'
+import Popup from 'reactjs-popup'
 import './Footer.css'
 import Demarche from "./img/demarche-ecologique.png"
 
 
 class Footer extends React.Component {
 
-    handleEvent = (event) => {
-        alert("salut!")
-    }
-
     render() {
         return (
             <div className="footer">
                 <div className="contenu">
                     <span className="copyrigth">@2019 LE MAUVAIS COIN</span>
-                    <span className="mentionLégale" onClick={this.handleEvent}>-Mentions Illégales-</span>
+                    <Popup trigger={<span className="illegal">- Mentions Illégales -</span>} modal>
+                    {close => (
+            <div className="modal">
+            <a className="close" onClick={close}>
+                &times;
+            </a>
+            <div className="header"> Mentions Illégales </div>
+            <div className="content">
+                {" "}
+                Tout achat de service sur notre plateforme est totalement non garantis. Le planning de nos prestataires est complètement aléatoire.
+                <br />
+                Nous ne garantissons pas non plus un travail soigneux, cela dépend de la sensibilité de nos intervenant et il se peut qu'il y ai confusion des tâches (tuer les courses, kidnapper le ménage...).
+            </div>
+            </div>
+    )}
+  </Popup>
+);   
+                    
                 </div>
                 <a className="lienEco" href="https://www.generation-net.org/fr/demarche-ecologique.html"><img src={Demarche} alt="démarche écologique"></img></a>
             </div>
